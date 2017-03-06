ecbd1f4139a7dada2faba08e966c42bc6b4391c4
Shereen Finney <shereenfinney@gmail.com>
2017-03-06 13:44:54 +0000

    Pass first unit test for user story 1

--- spec/bankAccount_spec.rb
+++ spec/bankAccount_spec.rb
@@ -1,5 +1,11 @@
 require "bankAccount"
 
 describe BankAccount do
+  describe "::new" do
+    subject { BankAccount.new }
 
+    it "creates a new bank account" do
+      expect(subject).to be_instance_of(BankAccount)
+    end
+  end
 end
